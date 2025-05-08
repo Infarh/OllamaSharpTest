e0a8fb84dc705dcbf43789d90db0ec25cd580c5b
Infarh <shmachilin@gmail.com>
2025-05-08 14:36:35 +0300

    Обратный вызов локальных функций со стороны LLM

--- OllamaTest/Program.cs
+++ OllamaTest/Program.cs
@@ -1,29 +1,32 @@
-﻿using System.Text;
+﻿using OllamaSharp;
 
-using OllamaSharp;
+using OllamaTest;
 
 const string url = "http://localhost:11434";
-const string model = "gemma3:27b";
+const string model = "mistral:latest";
+//const string model = "llama3.1:8b";
+//const string model = "llama3.2";
+//const string model = "gemma3:27b";
 
-using (var ollama2 = new OllamaApiClient(url, model))
-{
-    var result = new StringBuilder();
-    var request = ollama2.GenerateAsync(new()
-    {
-        Stream = false,
-        System = "Ты доктор филологии русского языка. Ты сделаешь порученную тебе работу и не будешь писать никакого другого текста кроме запрошенного.",
-        Prompt = "Просклоняй имя человека `Кошельков В.С.`. Создай список из значений: название падежа:значение. Каждый элемент списка должен быть на новой строке. Сохрани инициалы.",
-    });
+//using (var ollama2 = new OllamaApiClient(url, model))
+//{
+//    var result = new StringBuilder();
+//    var request = ollama2.GenerateAsync(new()
+//    {
+//        Stream = false,
+//        System = "Ты доктор филологии русского языка. Ты сделаешь порученную тебе работу и не будешь писать никакого другого текста кроме запрошенного.",
+//        Prompt = "Просклоняй имя человека `Кошельков В.С.`. Создай список из значений: название падежа:значение. Каждый элемент списка должен быть на новой строке. Сохрани инициалы.",
+//    });
 
-    await foreach (var r in request)
-        if (r is { Response: { Length: > 0 } response })
-        {
-            Console.Write(response);
-            result.AppendLine(response);
-        }
+//    await foreach (var r in request)
+//        if (r is { Response: { Length: > 0 } response })
+//        {
+//            Console.Write(response);
+//            result.AppendLine(response);
+//        }
 
-    var str = result.ToString();
-}
+//    var str = result.ToString();
+//}
 
 using var ollama = new OllamaApiClient(url, model);
 
@@ -38,15 +41,17 @@ foreach (var m in models)
 await foreach (var stream in ollama.GenerateAsync("Как у тебя дела?"))
     Console.Write(stream?.Response);
 
-var chat = new Chat(ollama);
+var chat = new Chat(ollama, "Ты мой персональный помощник. Мы общаемся с тобой по-русски. Будь лаконичен. Не пиши мне линей информации если она не была мной запрошена.");
 
 while (true)
 {
+    if (Console.CursorLeft > 0)
+        Console.WriteLine();
     Console.Write(">> ");
     if (Console.ReadLine() is not { Length: > 0 } message)
         break;
 
-    await foreach (var answer in chat.SendAsync(message))
+    await foreach (var answer in chat.SendAsync(message, [new GetWeatherTool()]))
         Console.Write(answer);
 }
 
---
+++ OllamaTest/WeatherTool.cs
@@ -0,0 +1,43 @@
+﻿using OllamaSharp;
+using OllamaSharp.Models.Chat;
+
+namespace OllamaTest;
+
+
+internal class WeatherTool : Tool
+{
+    public WeatherTool()
+    {
+        Function = new()
+        {
+            Description = "Получает погоду для выбранного города",
+            Name = "get_current_weather",
+            Parameters = new()
+            {
+                Properties = new()
+                {
+                    ["city"] = new() { Type = "string", Description = "Название города" }
+                },
+                Required = ["city"],
+                //Type = "object",
+            },
+        };
+        Type = "function";
+    }
+}
+
+internal class SampleTools
+{
+    [OllamaTool]
+    public static string GetWeather(string city, TemperatureUnit unit = TemperatureUnit.Celsius)
+    {
+        Console.WriteLine($" ==>> Запрос погоды от LLM для города {city} ид.изм {unit}");
+        return $"Погода в {city} - солнечно, 25 градусов {unit}.";
+    }
+
+    public enum TemperatureUnit
+    {
+        Celsius,
+        Fahrenheit,
+    }
+}
